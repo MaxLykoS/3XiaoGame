ee2d15968d658b5712349c677b167a9b8de92d5a
MaxLykoS <MaxLykoS@gmail.com>
2017-11-01 09:23:23 +0800

    XiaoXiaoLe - 2017.10.28
    安卓路径bugfix，还剩下UI的锚点

---
+++ Assets/Scripts/Levels/LevelBGUIAdjustSelf.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class LevelBGUIAdjustSelf : MonoBehaviour, IUIAdjustSelf {
+    public RectTransform Self { get; set; }
+
+    public RectTransform Target { get;set ;}
+
+    private Vector2 Pos;
+    public RectTransform spawnpoint;
+    public void GetTargetTransform()
+    {
+        Pos = new Vector2(GridController.Instance.xl*65, GridController.Instance.yl*65);
+        Self = GameObject.Find("Canvas/PlayGame/BG1").GetComponent<RectTransform>();
+        
+    }
+
+    public void SynSelfTransform()
+    {
+        Self.sizeDelta = Pos;
+        spawnpoint.localPosition = new Vector2(Self.position.x - Self.sizeDelta.x / 2 + 70, Self.position.y - Self.sizeDelta.y / 2 + 50);
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        GetTargetTransform();
+        SynSelfTransform();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
